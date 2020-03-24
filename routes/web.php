fde32a3ae692494ad397b83bd0ea93f0720a9959
Sherzod Usmonov <sherzod.usmon.91@gmail.com>
2020-03-25 03:49:10 +0500

    installed adminlte

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function() {
+    return redirect('login');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/dashboard', 'Admin\DashboardController@index')->name('dashboard');
+});
