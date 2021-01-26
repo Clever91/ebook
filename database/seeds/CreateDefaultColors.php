e5d5fdd70f295a80fcd1a9a639e6b146291b2cac
Sherzod Usmonov <sherzod.usmon.91@gmail.com>
2021-01-27 04:41:19 +0500

    fiz

--- database/seeds/CreateDefaultColors.php
+++ database/seeds/CreateDefaultColors.php
@@ -15,22 +15,22 @@ class CreateDefaultColors extends Seeder
     public function run()
     {
         $colors = [
-            [ 'name' => "Red", 'short' => "red", 'hex' => "#FF0000" ],
-            [ 'name' => "White", 'short' => "white", 'hex' => "#FFFFFF" ],
+            [ 'name' => "Red", 'short' => "🔴", 'hex' => "#FF0000" ],
+            [ 'name' => "White", 'short' => "⚪️", 'hex' => "#FFFFFF" ],
             [ 'name' => "Silver", 'short' => "silver", 'hex' => "#Silver" ],
             [ 'name' => "Gray", 'short' => "gray", 'hex' => "#808080" ],
-            [ 'name' => "Black", 'short' => "black", 'hex' => "#000000" ],
+            [ 'name' => "Black", 'short' => "⚫️", 'hex' => "#000000" ],
             [ 'name' => "Maroon", 'short' => "maroon", 'hex' => "#800000" ],
-            [ 'name' => "Yellow", 'short' => "yellow", 'hex' => "#FFFF00" ],
+            [ 'name' => "Yellow", 'short' => "🟡", 'hex' => "#FFFF00" ],
             [ 'name' => "Olive", 'short' => "olive", 'hex' => "#808000" ],
             [ 'name' => "Lime", 'short' => "lime", 'hex' => "#00FF00" ],
-            [ 'name' => "Green", 'short' => "green", 'hex' => "#008000" ],
+            [ 'name' => "Green", 'short' => "🟢", 'hex' => "#008000" ],
             [ 'name' => "Aqua", 'short' => "aqua", 'hex' => "#00FFFF" ],
             [ 'name' => "Teal", 'short' => "teal", 'hex' => "#008080" ],
-            [ 'name' => "Blue", 'short' => "blue", 'hex' => "#0000FF" ],
+            [ 'name' => "Blue", 'short' => "🔵", 'hex' => "#0000FF" ],
             [ 'name' => "Navy", 'short' => "navy", 'hex' => "#000080" ],
             [ 'name' => "Fuchsia", 'short' => "fuchsia", 'hex' => "#FF00FF" ],
-            [ 'name' => "Purple", 'short' => "purple", 'hex' => "#800080" ],
+            [ 'name' => "Purple", 'short' => "🟣", 'hex' => "#800080" ],
         ];
 
         foreach($colors as $item) {
