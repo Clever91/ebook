570dfc1fddcad508428b4b76beea6055551904d9
Sherzod Usmonov <sherzod.usmon.91@gmail.com>
2021-02-12 04:43:58 +0500

    fix

--- resources/views/layouts/leftsidebar.blade.php
+++ resources/views/layouts/leftsidebar.blade.php
@@ -1,9 +1,13 @@
+@php
+use App\Models\Admin\Setting;
+@endphp
+
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('dashboard') }}" class="brand-link elevation-4">
         <img src="{{ asset('images/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">{{ env('APP_NAME') }}</span>
+        <span class="brand-text font-weight-light">{{ Setting::get('app_name') }}</span>
     </a>
 
     <!-- Sidebar -->
