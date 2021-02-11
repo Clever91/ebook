6932f0e807f456a34a5e96f3287b093167a64648
Sherzod Usmonov <sherzod.usmon.91@gmail.com>
2021-02-12 04:38:29 +0500

    fix

--- resources/views/layouts/leftsidebar.blade.php
+++ resources/views/layouts/leftsidebar.blade.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="{{ route('dashboard') }}" class="brand-link elevation-4">
         <img src="{{ asset('images/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">Book Media Nashr</span>
+        <span class="brand-text font-weight-light">{{ env('APP_NAME') }}</span>
     </a>
 
     <!-- Sidebar -->
