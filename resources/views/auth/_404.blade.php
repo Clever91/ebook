296d5b2e2132c2d15ddc063241611a3304aed44a
Sherzod Usmonov <sherzod.usmon.91@gmail.com>
2021-06-27 11:29:05 +0500

    404 error

--- resources/views/auth/_404.blade.php
+++ resources/views/auth/_404.blade.php
@@ -1,28 +1,24 @@
-@extends('layouts.auth.login')
+@extends('layouts.front.front')
 
-@section('title', 'Ошибка')
+@section('title', 'Errro 404')
 
 @section('content')
 
-
-<!-- Main content -->
-<section class="content">
-    <div class="error-page">
-        <h2 class="headline text-warning"> 404</h2>
-
-        <div class="error-content">
-            <h3>
-                <i class="fas fa-exclamation-triangle text-warning"></i> К сожалению! Страница не найдена.
-            </h3>
-            <p>
-                Мы не смогли найти страницу, которую вы искали.
-                Вы можете вернуться <a href="{{ route('dashboard') }}">на панель инструментов</a>
-            </p>
+<!-- ====== MAIN CONTENT ===== -->
+<main id="content">
+    <div class="container">
+        <div class="space-bottom-1 space-top-xl-2 space-bottom-xl-4">
+            <div class="d-flex flex-column align-items-center pt-lg-7 pb-lg-4 pb-lg-6">
+                <div class="font-weight-medium font-size-200 font-size-xs-170 text-lh-sm mt-xl-1">404</div>
+                <h6 class="font-size-4 font-weight-medium mb-2">Woops, looks like this page does not exist</h6>
+                <span class="font-size-2 mb-6">You could either go back or go to homepage</span>
+                <div class="d-flex align-items-center flex-column">
+                    <a href="{{ route('front.home') }}" class="btn btn-dark rounded-0 btn-wide height-60 width-250 font-weight-medium">Go Home</a>
+                </div>
+            </div>
         </div>
-        <!-- /.error-content -->
-      </div>
-    <!-- /.error-page -->
-</section>
-<!-- /.content -->
+    </div>
+</main>
+<!-- ====== END MAIN CONTENT ===== -->
 
 @endsection
